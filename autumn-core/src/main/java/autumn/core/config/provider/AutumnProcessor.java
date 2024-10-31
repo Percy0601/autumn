e8a17d8afb5322cceb44034f0942ccea3cb10a6e
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-10-31 11:31:54 +0100

    update

--- autumn-core/src/main/java/autumn/core/config/provider/AutumnProcessor.java
+++
@@ -1,9 +0,0 @@
-package autumn.core.config.provider;
-
-import org.apache.thrift.TProcessor;
-
-public interface AutumnProcessor {
-
-    TProcessor multiplexedProcessor();
-
-}
---
+++ autumn-core/src/main/java/autumn/core/server/AutumnConsumer.java
@@ -0,0 +1,11 @@
+package autumn.core.server;
+
+/**
+ * 
+ * @author: baoxin.zhao
+ * @date: 2024/10/31 
+ */
+
+
+public class AutumnConsumer {
+}
