f5e6716fd586b57ce6b403a6ee00f1cc5caef3f2
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-11-08 14:49:18 +0000

    update

---
+++ autumn-spring-boot/autumn-spring-boot-autoconfigure/src/main/java/com/microapp/autumn/boot/config/AutumnAutoConfig.java
@@ -0,0 +1,11 @@
+package com.microapp.autumn.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/11/8
+ */
+@Configuration
+public class AutumnAutoConfig {
+}
---
+++ autumn-spring-boot/autumn-spring-boot-autoconfigure/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author: baoxin.zhao
+ * @date: 2024/11/8 
+ */
+module autumn.spring.boot.starter {
+    requires lombok;
+    requires org.slf4j;
+    requires transitive java.compiler;
+    requires transitive autumn.core;
+    requires spring.context;
+
+}
