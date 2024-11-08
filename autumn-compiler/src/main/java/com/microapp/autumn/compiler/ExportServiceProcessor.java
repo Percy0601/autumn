cb5199e279fbaf88101ab10174fef8dd42ad7b5f
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-11-08 11:12:22 +0000

    update

--- autumn-compiler/src/main/java/com/microapp/autumn/compiler/ExportServiceProcessor.java
+++ autumn-compiler/src/main/java/com/microapp/autumn/compiler/ExportServiceProcessor.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
@@ -12,13 +15,30 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
 import java.util.List;
 import java.util.Set;
 
-@SupportedAnnotationTypes(value = {"autumn.core.annotation.Export"})
-@SupportedSourceVersion(value = SourceVersion.RELEASE_11)
+@SupportedAnnotationTypes(value = {"com.microapp.autumn.api.annotation.Export"})
+@SupportedSourceVersion(value = SourceVersion.RELEASE_17)
 public class ExportServiceProcessor extends AbstractProcessor {
     private Logger log = LoggerFactory.getLogger(ExportServiceProcessor.class);
+
+    private Filer mFilerUtils;
+    private Types mTypesUtils;
+    private Elements mElementsUtils;
+    private Messager mMessager;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+        mFilerUtils = processingEnv.getFiler();
+        mTypesUtils = processingEnv.getTypeUtils();
+        mElementsUtils = processingEnv.getElementUtils();
+        mMessager = processingEnv.getMessager();
+    }
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (TypeElement annotationElement: annotations) {
